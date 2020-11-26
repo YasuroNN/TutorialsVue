d63e0c28920d4d465dfe50c9e9bb57c333e5e43f
yasuro <voronov.maksim.v@gmail.com>
2020-11-26 18:16:21 +0300

    components

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <h1>Parent: {{ pizzaName }}</h1>
+    <app-pizza :pizzaName="pizzaName" 
+    :pizzaPrice="pizzaPrice"
+    :changeFunc="changeNameToPizza"
+    @nameChanged="pizzaName = $event"
+    @counterUpdated="counter =  $event"
+    ></app-pizza>
+
+    <app-counter></app-counter>
+  </div>
+</template>
+
+<script>
+import Pizza from "./Pizza.vue";
+import Counter from "./Counter.vue"
+export default {
+  name: "app",
+  data() {
+    return {
+      pizzaName: "Derevensay from app.vue",
+      pizzaPrice: 480
+    };
+  },
+  components: {
+    appPizza: Pizza,
+    appCounter: Counter
+  },
+
+  methods: {
+    changeNameToPizza() {
+      this.pizzaName = "Pizza"
+    }
+  }
+};
+</script>
